accc01ceaf9ca78c19438d1f36dd6a5cd4c73f22
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-08-10 21:11:32 +0000

    average-weighted-score-users

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -7,7 +7,7 @@ BEGIN
 	UPDATE users AS U,
 	    (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg
 	     FROM users AS U
-	     JOIN corrections as C ON U.id=user_id
+	     JOIN corrections as C ON U.id=C.user_id
 	     JOIN projects AS P ON C.project_id=P.id
 	     GROUP BY U.id)
 	AS WA
