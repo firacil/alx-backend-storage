49288f5ac78700ce20868089761bd269575de97c
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-08-14 20:48:43 +0000

    redis basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,6 +22,48 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """store the history of inputs and outputs"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper of decorated function"""
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method.__qualname__ + ":outputs", output)
+        return output
+
+    return wrapper
+
+
+def replay(fn: Callable):
+    """display the history of calls"""
+    r = redis.Redis()
+    fname = fn.__qualname__
+    value = r.get(fname)
+    try:
+        value = int(value.decode("utd-8"))
+    except Exception:
+        value = 0
+
+    print("{} was called {} times:".format(fname, value))
+    inputs = r.lrange("{}:inputs".format(fname), 0, -1)
+
+    outputs = r.lrange("{}:outputs".format(fname), 0, -1)
+
+    for input, output in zip(inputs, outputs):
+        try:
+            input = input.decode("utf-8")
+        except Exception:
+            input = ""
+
+        try:
+            output = output.decode("utf-8")
+        except Exception:
+            output = ""
+
+        print("{}(*{}) -> {}".format(fname, input, output))
+
+
 class Cache:
     """class cache"""
     def __init__(self):
@@ -30,6 +72,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the data in redis with a random key
 
